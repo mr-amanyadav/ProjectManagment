910d189416fe3081a5f93d0d98dca92f221f73b5
Aman <mr.amanyadav18082003@gmail.com>
2026-07-09 09:32:03 +0530

    validate login and test login also add cookies.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,20 @@
 import {Router} from "express"
 
-import { registerUser } from "../controllers/auth.controllers.js";
+import { registerUser , login} from "../controllers/auth.controllers.js";
+
+import { validate } from "../middlewares/validator.middleware.js";
+
+import { userRegisterValidator , userLoginValidator} from "../validators/index.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiError } from "../utils/api-error.js";
 
 
 
 const router=Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(userRegisterValidator(), validate , registerUser);
 
-
-
+router.route("/login").post(userLoginValidator(),validate,login);
 
 
 export default router;
